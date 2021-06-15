a3bb061b7b424a2ad47df26b028f949afba03286
gunes-dot <bilun.gunes@gmail.com>
2021-06-15 23:23:46 +0300

    forth commit

--- src/pet.js
+++ src/pet.js
@@ -1,4 +1,36 @@
-function Pet() {}
+function Pet(petname) {
+    this.name = petname;
+    this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
+
+
+}
+
+Pet.prototype.growUp = function growUp() {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+};
+
+const MaxFitness = 10
+
+Pet.prototype.walk = function walk() {
+    this.fitness += 4;
+    if (this.fitness > MaxFitness) {
+        this.fitness = MaxFitness;
+    }
+};
+
+const MinHunger = 0
+
+Pet.prototype.feed = function feed () {
+    this.hunger -= 3;
+    if (this.hunger < MinHunger) {
+        this.hunger = MinHunger;
+    }
+}
+
 
 module.exports = Pet;
 
